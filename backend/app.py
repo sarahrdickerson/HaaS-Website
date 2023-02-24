d32fa0d666f63694bacca856e58281909c9e8ef2
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-02-23 18:11:36 -0600

    Removed __pycache__ from git tracking

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,26 @@
-# to run type "flask run"
+# make sure you are in the backend folder in cmd
+# create myenv or enter existing one (to enter existing one, skip to activate step)
+# python -m venv myenv
+# install all packages 
+# pip install -r requirements.txt
+# activate 
+# .\myenv\Scripts\activate
+# flask run
+# http://localhost:5000/userdata to see data
+
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb+srv://gabrielaperezgil:ECE461L@cluster0.5v3hp19.mongodb.net/mydatabase'
+app.config['MONGO_URI'] = 'mongodb+srv://gabrielaperezgil:ECE461L@cluster0.5v3hp19.mongodb.net/Existing_Users'
 mongo = PyMongo(app)
 
-@app.route('/data')
+@app.route('/userdata')
+# this is a simple API that returns User Information data
+# this will be called by the react front end
 def get_data():
-    collection = mongo.db.mycollection
+    collection = mongo.db.userInfo
     data = []
 
     for document in collection.find():
