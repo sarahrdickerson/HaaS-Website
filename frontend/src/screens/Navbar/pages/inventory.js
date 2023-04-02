a0c4762d1d60332c11d6c1342029353ef434075f
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-04-01 22:31:47 -0500

    Inventory prompts with database availability now

--- frontend/src/screens/Navbar/pages/inventory.js
+++ frontend/src/screens/Navbar/pages/inventory.js
@@ -1,45 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './inventory.css';
+import axios from "../../../api/axios";
 
 function Inventory() {
-  const [hwSet1Availability, setHwSet1Availability] = useState(100);
-  const [hwSet2Availability, setHwSet2Availability] = useState(100);
+  const [hwSet1Availability, setHwSet1Availability] = useState(null);
+  const [hwSet2Availability, setHwSet2Availability] = useState(null);
   const [hwSet1Input, setHwSet1Input] = useState("");
   const [hwSet2Input, setHwSet2Input] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const handleCheckIn = (set, input) => {
-  const capacity = set === "hwSet1" ? 100 : 100; // Update capacity as needed
-  const availability = set === "hwSet1" ? hwSet1Availability : hwSet2Availability;
-  if (parseInt(input) > capacity - availability) {
-    setErrorMessage(`Please try again with a lower quantity (capacity: ${capacity}, availability: ${availability})`);
-    return;
-  }
-  if (set === "hwSet1") {
-    setHwSet1Availability((prev) => prev + parseInt(input));
-    setHwSet1Input("");
-  } else {
-    setHwSet2Availability((prev) => prev + parseInt(input));
-    setHwSet2Input("");
-  }
-  setErrorMessage("");
-};
+  useEffect(() => {
+    async function fetchAvailability() {
+      try {
+        const response = await axios.get("/api/availability");
+        setHwSet1Availability(response.data.HWSet1_available);
+        setHwSet2Availability(response.data.HWSet2_available);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchAvailability();
+  }, []);
 
-const handleCheckOut = (set, input) => {
-  const availability = set === "hwSet1" ? hwSet1Availability : hwSet2Availability;
-  if (parseInt(input) > availability) {
-    setErrorMessage(`Not enough available, please enter a lower quantity (availability: ${availability})`);
-    return;
-  }
-  if (set === "hwSet1") {
-    setHwSet1Availability((prev) => prev - parseInt(input));
-    setHwSet1Input("");
-  } else {
-    setHwSet2Availability((prev) => prev - parseInt(input));
-    setHwSet2Input("");
-  }
-  setErrorMessage("");
-};
+  const handleCheckIn = (set, input) => {
+    const capacity = set === "hwSet1" ? 100 : 100; // Update capacity as needed
+    const availability = set === "hwSet1" ? hwSet1Availability : hwSet2Availability;
+    if (parseInt(input) > capacity - availability) {
+      setErrorMessage(`Please try again with a lower quantity (capacity: ${capacity}, availability: ${availability})`);
+      return;
+    }
+    if (set === "hwSet1") {
+      setHwSet1Availability((prev) => prev + parseInt(input));
+      setHwSet1Input("");
+    } else {
+      setHwSet2Availability((prev) => prev + parseInt(input));
+      setHwSet2Input("");
+    }
+    setErrorMessage("");
+  };
+
+  const handleCheckOut = (set, input) => {
+    const availability = set === "hwSet1" ? hwSet1Availability : hwSet2Availability;
+    if (parseInt(input) > availability) {
+      setErrorMessage(`Not enough available, please enter a lower quantity (availability: ${availability})`);
+      return;
+    }
+    if (set === "hwSet1") {
+      setHwSet1Availability((prev) => prev - parseInt(input));
+      setHwSet1Input("");
+    } else {
+      setHwSet2Availability((prev) => prev - parseInt(input));
+      setHwSet2Input("");
+    }
+    setErrorMessage("");
+  };
 
 
   const handleHwSet1InputChange = (event) => {
@@ -64,20 +78,20 @@ const handleCheckOut = (set, input) => {
           onChange={handleHwSet1InputChange}
         />
         <button
-        className="button"
-              sx={{
-                backgroundColor: 'lightgray',
-                color: 'black',
-                textTransform: 'none',
-                borderRadius: '0',
-                width: '100px',
-                height: '50px',
-                fontSize: '1.2rem',
-                marginLeft: '8px',
-                fontWeight: 'bold',
-                textAlign: 'center'
-              }}
-        onClick={() => handleCheckIn("hwSet1", hwSet1Input)}
+          className="checkin-button"
+          sx={{
+            backgroundColor: 'lightgray',
+            color: 'black',
+            textTransform: 'none',
+            borderRadius: '0',
+            width: '100px',
+            height: '50px',
+            fontSize: '1.2rem',
+            marginLeft: '8px',
+            fontWeight: 'bold',
+            textAlign: 'center'
+          }}
+          onClick={() => handleCheckIn("hwSet1", hwSet1Input)}
         >
           Check In
         </button>
